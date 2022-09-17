aeb66c3e90d113717f60afb2333d16968c6da465
marinescuana11 <marinescuana28@gmail.com>
2022-09-17 16:48:38 +0300

    Add swagger [simple version]

--- AttendanceManager/AttendanceManager.Api/Startup.cs
+++ AttendanceManager/AttendanceManager.Api/Startup.cs
@@ -4,6 +4,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using AttendanceManager.Persistance;
+using Microsoft.OpenApi.Models;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace AttendanceManager.Api
 {
     public class Startup
@@ -18,9 +22,11 @@ namespace AttendanceManager.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            //add this to configure Swagger
+            AddSwagger(services);
 
             services.AddDbConnectionServices(Configuration);
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,11 +41,30 @@ namespace AttendanceManager.Api
 
             app.UseRouting();
 
+            app.UseAuthentication();
+
+            //add this to configure Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Attendance Manager API");
+            });
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+        }
+
+        private void AddSwagger(IServiceCollection services)
+        {
+            //For SWAGGER
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Core API", Description = "Swagger Core API" });
+                c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
             });
         }
     }
