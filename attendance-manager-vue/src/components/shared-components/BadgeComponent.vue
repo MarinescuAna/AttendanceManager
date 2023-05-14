9233d1487a470c35c3bc4df153590158e13bf3f9
marinescuana11 <marinescuana28@gmail.com>
2023-05-14 16:48:21 +0300

    #refactor add some style to badges

--- attendance-manager-vue/src/components/shared-components/BadgeComponent.vue
+++ attendance-manager-vue/src/components/shared-components/BadgeComponent.vue
@@ -1,18 +1,46 @@
 <template>
-  <v-card width="150" class="ma-2">
-    <v-img 
-      width="150"
-      height="100"
-      max-height="100"
-      max-width="150"
-      :src="require(`@/assets/images/badges/${badge.imagePath}`)"
-    ></v-img>
-    <v-card-subtitle> 
-        {{ badge.title }}
-    </v-card-subtitle>
-  </v-card>
+  <v-tooltip bottom>
+    <template v-slot:activator="{ on, attrs }">
+      <v-layout
+        class="badge-container"
+        v-bind="attrs"
+        v-on="on"
+        align-center
+        justify-center
+        column
+      >
+        <v-img
+          class="image-display"
+          :src="require(`@/assets/images/badges/${badge.imagePath}`)"
+        ></v-img>
+        <span class="title">
+          {{ badge.title }}
+        </span>
+      </v-layout>
+    </template>
+    <span>{{ tooltip }}</span>
+  </v-tooltip>
 </template>
 
+<style scoped>
+.badge-container {
+  width: 130px;
+}
+.image-display {
+  width: 130px;
+  height: 130px;
+  object-fit: cover;
+  border-radius: 50%;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.7);
+}
+
+.title {
+  font-size: 15px !important;
+  font-family: cursive !important;
+  font-weight: bold;
+  text-align: center;
+}
+</style>
 <script lang="ts">
 import Vue from "vue";
 import { BadgeViewModule } from "@/modules/document";
@@ -23,6 +51,15 @@ export default Vue.extend({
     badge: {
       required: true,
       type: Object as () => BadgeViewModule,
+    },
+    details: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {
+    tooltip: function (): string {
+      return this.details === "" ? this.badge.title : this.details;
     },
   },
 });
