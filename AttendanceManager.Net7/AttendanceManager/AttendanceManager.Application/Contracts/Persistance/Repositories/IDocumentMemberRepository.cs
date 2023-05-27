fb9808d2056fa6206cd677934decde234807523d
marinescuana11 <marinescuana28@gmail.com>
2023-05-27 20:16:42 +0300

    #refactor remove unused methods form member repo

--- AttendanceManager.Net7/AttendanceManager/AttendanceManager.Application/Contracts/Persistance/Repositories/IDocumentMemberRepository.cs
+++ AttendanceManager.Net7/AttendanceManager/AttendanceManager.Application/Contracts/Persistance/Repositories/IDocumentMemberRepository.cs
@@ -6,7 +6,6 @@ namespace AttendanceManager.Application.Contracts.Persistance.Repositories
     public interface IDocumentMemberRepository : IGenericRepository<DocumentMember>
     {
         Task<List<DocumentMember>> GetDocumentMembersByDocumentIdAndRoleAsync(int documentId, Role? role);
-        Task<DocumentMember?> GetMemberByDocumentIdAndUserIdAsync(int documentId, string userId);
         void DeleteMembersByDocumentId(int documentId);
     }
 }
--- AttendanceManager.Net7/AttendanceManager/AttendanceManager.Persistance/Repositories/DocumentMemberRepository.cs
+++ AttendanceManager.Net7/AttendanceManager/AttendanceManager.Persistance/Repositories/DocumentMemberRepository.cs
@@ -12,13 +12,6 @@ namespace AttendanceManager.Persistance.Repositories
         {
         }
 
-        public async Task<DocumentMember?> GetMemberByDocumentIdAndUserIdAsync(int documentId, string userId)
-            => await dbContext.DocumentMembers
-            .Include(dm => dm.User!.Attendances)
-            .Include(dm => dm.Document!.AttendanceCollections)
-            .AsNoTracking()
-            .FirstOrDefaultAsync(dm => dm.UserID == userId && dm.DocumentID == documentId);
-
         /// <summary>
         /// This is a list with all the members of a given document. The role of the student can be passed as parameter, or can be null and in this case
         /// all the members of the document are returned
