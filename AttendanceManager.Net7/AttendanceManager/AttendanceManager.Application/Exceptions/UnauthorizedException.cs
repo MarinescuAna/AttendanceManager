13f049adc2ed7b77f39008526a9807bc35dc2d26
marinescuana11 <marinescuana28@gmail.com>
2023-05-28 07:48:48 +0300

    #refactor add exception

---
+++ AttendanceManager.Net7/AttendanceManager/AttendanceManager.Application/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,10 @@
+﻿namespace AttendanceManager.Application.Exceptions
+{
+    public sealed class UnauthorizedException : ApplicationException
+    {
+        public UnauthorizedException(string name)
+            : base(name)
+        {
+        }
+    }
+}
