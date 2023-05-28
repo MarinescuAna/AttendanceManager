261a13ed0da8ac00cff6d41c53e8589a2f74760d
marinescuana11 <marinescuana28@gmail.com>
2023-05-28 07:49:39 +0300

    #refactor document members style changes

--- attendance-manager-vue/src/components/document-components/tabs/DocumentMembersComponent.vue
+++ attendance-manager-vue/src/components/document-components/tabs/DocumentMembersComponent.vue
@@ -7,43 +7,51 @@
           :items="members"
           item-key="email"
           dense
-          class="elevation-1"
+          class="table-color"
+          v-if="members.length != 0"
         >
         </v-data-table>
+        <div v-else>
+          <MessageComponent description="There is no collaborators added yet." color="transparent"/>
+        </div>
       </v-flex>
     </v-layout>
   </div>
 </template>
-
+<style scoped>
+.table-color {
+  background-color: transparent;
+}
+</style>
 <script lang="ts">
 import Vue from "vue";
 import storeHelper from "@/store/store-helper";
 import { DocumentMembersViewModule } from "@/modules/document";
+import MessageComponent from "@/components/shared-components/MessageComponent.vue";
 
 export default Vue.extend({
-  name: "DocumentMembersComponent",
-  data() {
-    return {
-      header: [
-        {
-            text:"Email",
-            value:'email'
-        }, 
-        {
-            text:"Fullname",
-            value:'name'
-        }, 
-        {
-            text:"Role",
-            value:'role'
-        }, 
-      ],
-    };
-  },
-  computed: {
-    members: function (): DocumentMembersViewModule[] {
-      return storeHelper.documentStore.documentDetails.documentMembers;
+    name: "DocumentMembersComponent",
+    data() {
+        return {
+            header: [
+                {
+                    text: "Email",
+                    value: "email",
+                    class: "text-left black--text text-h6"
+                },
+                {
+                    text: "Fullname",
+                    value: "name",
+                    class: "text-left black--text text-h6"
+                },
+            ],
+        };
     },
-  },
+    computed: {
+        members: function (): DocumentMembersViewModule[] {
+            return storeHelper.documentStore.documentDetails.documentMembers;
+        },
+    },
+    components: { MessageComponent }
 });
 </script>
