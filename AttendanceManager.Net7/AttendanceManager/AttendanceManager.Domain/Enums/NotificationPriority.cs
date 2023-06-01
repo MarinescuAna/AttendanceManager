a49eae091b205969933e5182c4239c2eef277654
marinescuana11 <marinescuana28@gmail.com>
2023-06-01 11:15:51 +0300

    #refactor clean up  enum

--- AttendanceManager.Net7/AttendanceManager/AttendanceManager.Domain/Enums/NotificationPriority.cs
+++ AttendanceManager.Net7/AttendanceManager/AttendanceManager.Domain/Enums/NotificationPriority.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AttendanceManager.Domain.Enums
+﻿namespace AttendanceManager.Domain.Enums
 {
     public enum NotificationPriority
     {
