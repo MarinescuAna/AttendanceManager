796a385f90e54c373c465259d434382c82c0d3a9
marinescuana11 <marinescuana28@gmail.com>
2023-06-03 23:37:36 +0300

    #implementation add not found

--- attendance-manager-vue/src/views/routing-views/NotFoundView.vue
+++ attendance-manager-vue/src/views/routing-views/NotFoundView.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>
-    <h4>The page your are looking for could not be found</h4>
-    <div>
-      {{ $route.params.pathMatch }}
-    </div>
-  </div>
+  <v-layout class="ma-3" column>
+      <h1 class="font-weight-normal">
+        <i> {{ $route.params.pathMatch }}</i> could not be found!
+      </h1>
+      <v-img
+        :src="require(`@/assets/images/errors/not_found.png`)"
+        class="ma-3"
+        width="30%"
+      ></v-img>
+  </v-layout>
 </template>
