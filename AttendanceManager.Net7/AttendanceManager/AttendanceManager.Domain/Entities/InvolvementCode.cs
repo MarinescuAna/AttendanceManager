b7e7f577760997137902d79618a9234c6eb7b95b
marinescuana11 <marinescuana28@gmail.com>
2023-06-25 20:29:40 +0300

    #bug add FK

--- AttendanceManager.Net7/AttendanceManager/AttendanceManager.Domain/Entities/InvolvementCode.cs
+++ AttendanceManager.Net7/AttendanceManager/AttendanceManager.Domain/Entities/InvolvementCode.cs
@@ -1,13 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AttendanceManager.Domain.Entities
 {
-    public sealed class InvolvementCode
+    public class InvolvementCode
     {
         [MaxLength(8)]
         [Key]
         public required string Code { get; set; }
         public required DateTime ExpirationDate { get; set; }
+        [ForeignKey("Collection")]
         public required int CollectionID { get; set; }
+        public virtual Collection? Collection { get; set; }
     }
 }
